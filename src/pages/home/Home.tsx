b771bef6d9499c4a852d50f4f1118747432fd00d
EDusik <eduardodusik@gmail.com>
2022-10-22 18:44:50 -0300

    style: add home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { HomeStyled } from "pages/home/HomeStyle";
 
 export const Home = () => {
 	return (
-		<React.Fragment>
-			<h1>Home</h1>
-		</React.Fragment>
+		<HomeStyled>
+			<div className="home">
+				<p>Please, enter your full name below</p>
+				<p>Only alphabetical characters are accepted</p>
+				<input className="home__input-name" type="text" id="full-name" name="full-name" placeholder="Full name" />
+
+				<div className="home__input-checkbox">
+					<input type="checkbox" id="checkbox" name="checkbox" />
+					<label htmlFor="checkbox">Are you older than 18 years old?</label>
+				</div>
+
+				<button type="button" aria-label="Enter Button">
+					Enter
+				</button>
+			</div>
+			<img src={`${process.env.PUBLIC_URL}/images/bee.svg`} alt="Bee Logo" />
+		</HomeStyled>
 	);
 };
---
+++ src/pages/home/HomeStyle.ts
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const HomeStyled = styled.div`
+	margin: 0 auto;
+	max-width: 100%;
+	min-height: 100vh;
+  background-color: var(--primary);
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+
+  div.home {
+    min-height: 225px;
+    align-items: center;
+    display: grid;
+    margin: 0 auto;
+    justify-items: start;
+
+    p, label {
+      font-size: 14px;
+    }
+
+    input.home__input-name {
+      font-size: 1rem;
+      height: 44px;
+      width: 300px;
+      padding: 10px 12px;
+      border: 1px solid var(--input-border);
+      border-radius: 4px;
+    }
+
+    div.home__input-checkbox {
+      justify-content: center;
+      display: flex;
+      width: 100%;
+
+      input {
+        margin-right: 12px;
+      }
+    }
+
+    button {
+      border: none;
+      padding: 8px 16px;
+      width: 75px;
+      height: 40px;
+      color: white; 
+      margin: 0 auto;
+      background-color: var(--button-enabled);
+      transition: .2s;
+
+      &:hover {
+        opacity: 0.9;
+      }
+      
+      &:disabled {
+        background-color: var(--button-disabled);
+        cursor: not-allowed;
+      }
+    }
+  }
+
+  img {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+`;
