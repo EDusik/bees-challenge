4380a4283826fa124f34fe0d85f3b063b4b9c3fd
EDusik <eduardodusik@gmail.com>
2022-10-22 20:53:07 -0300

    feat: add form to home page

--- src/pages/home/HomeStyle.ts
+++ src/pages/home/HomeStyle.ts
@@ -10,57 +10,6 @@ export const HomeStyled = styled.div`
   flex-wrap: wrap;
   align-items: center;
 
-  form.home {
-    min-height: 225px;
-    align-items: center;
-    display: grid;
-    margin: 0 auto;
-    justify-items: start;
-
-    p, label {
-      font-size: 14px;
-    }
-
-    input.home__input-name {
-      font-size: 1rem;
-      height: 44px;
-      width: 300px;
-      padding: 10px 12px;
-      border: 1px solid var(--input-border);
-      border-radius: 4px;
-    }
-
-    div.home__input-checkbox {
-      justify-content: center;
-      display: flex;
-      width: 100%;
-
-      input {
-        margin-right: 12px;
-      }
-    }
-
-    button {
-      border: none;
-      padding: 8px 16px;
-      width: 75px;
-      height: 40px;
-      color: white; 
-      margin: 0 auto;
-      background-color: var(--button-enabled);
-      transition: .2s;
-
-      &:hover {
-        opacity: 0.9;
-      }
-      
-      &:disabled {
-        background-color: var(--button-disabled);
-        cursor: not-allowed;
-      }
-    }
-  }
-
   img {
     position: absolute;
     bottom: 0;
