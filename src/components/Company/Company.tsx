b04676df3497d2cca1209ddb3f712e46ecb74e50
EDusik <eduardodusik@gmail.com>
2022-10-22 14:56:50 -0300

    refactor change company card:

--- src/components/Company/Company.tsx
+++ src/components/Company/Company.tsx
@@ -1,5 +1,6 @@
 import { ICompany } from "models/Companies";
 import { CompanyStyled } from "./CompanyStyled";
+import { handleNullData } from "components/utils/data";
 
 interface ICompanyProps {
 	company: ICompany;
@@ -8,20 +9,53 @@ interface ICompanyProps {
 export const Company = ({ company }: ICompanyProps) => {
 	const handleAddress = (company: ICompany) => {
 		return (
-			<>
+			<div>
 				<p>{company.street}</p>
 				<p>
 					{company.city}, {company.state} - {company.country}
 				</p>
-			</>
+
+				{handleNullData(
+					company?.brewery_type,
+					<span>
+						<img src={`${process.env.PUBLIC_URL}/images/chart.svg`} alt="Chart - Icon" />
+						{company?.brewery_type}
+					</span>
+				)}
+
+				{handleNullData(
+					company?.postal_code,
+					<span>
+						<img src={`${process.env.PUBLIC_URL}/images/pin.svg`} alt="Pin - Icon" />
+						{company?.postal_code}
+					</span>
+				)}
+
+				{handleNullData(
+					company?.phone,
+					<span>
+						<img src={`${process.env.PUBLIC_URL}/images/phone.svg`} alt="Phone - Icon" />
+						{company?.phone}
+					</span>
+				)}
+			</div>
 		);
+	};
+
+	const handleDelete = async (company: ICompany) => {
+		console.log(company);
 	};
 
 	return (
 		<CompanyStyled>
 			<div key={company.id}>
-				<h2>{company.id}</h2>
-				<p>{handleAddress(company)}</p>
+				<div className="company__title">
+					<h2>{company.id}</h2>
+					<button type="button" aria-label="Delete Button" onClick={() => handleDelete(company)}>
+						<img src={`${process.env.PUBLIC_URL}/images/trash.svg`} alt="Trash - Icon" />
+					</button>
+				</div>
+				{handleAddress(company)}
 			</div>
 		</CompanyStyled>
 	);
--- src/components/Company/CompanyStyled.ts
+++ src/components/Company/CompanyStyled.ts
@@ -2,16 +2,32 @@ import styled from "styled-components";
 
 export const CompanyStyled = styled.div`
   width: 382px;
-  min-height: 282px;
+  min-height: 300px;
   display: flex;
-  margin: 24px auto;
+  margin: 24px;
   padding: 30px 24px;
   background-color: var(--white);
   border: solid 1px var(--border);
 
-  h2  {
-    font-size: 20px;
-    font-weight: 700;
+
+  div.company__title {
+    display: flex;
+    justify-content: space-between;
+
+    h2  {
+      font-size: 20px;
+      font-weight: 700;
+    }
+
+    button {
+      background-color: transparent;
+      border: none;
+      transition: 0.2s;
+
+      &:hover {
+        opacity: .5;
+      }
+    }
   }
 
   p {
@@ -26,4 +42,23 @@ export const CompanyStyled = styled.div`
       margin-bottom: 30px;
     }
   }
+
+  span {
+    display: inline-flex;
+    background-color: var(--primary);
+    margin: 1rem 14px 1rem 0;
+    padding: 4px 16px;
+    border-radius: 1rem;
+    font-size: 16px;
+    max-width: 185px;
+
+    img {
+      width: 16px;
+      margin-right: 12px;
+    }
+  }
+
+	@media (max-width: 450px) {
+    width: calc(100% - 48px);
+	}
 `;
