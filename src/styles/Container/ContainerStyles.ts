b2bba3bc394240f2e8152f58bfa1952fe78641d8
EDusik <eduardodusik@gmail.com>
2022-10-22 19:46:08 -0300

    style*(container): change min-height

--- src/styles/Container/ContainerStyles.ts
+++ src/styles/Container/ContainerStyles.ts
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const ContainerStyled = styled.div`
 	margin: 0 auto;
 	max-width: 1290px;
-	min-height: 100vh;
+  min-height: 100vh;
+  min-height: calc(100vh - 146px);
 `;
